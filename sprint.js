5f69db9b98dc0ef54b78aca57f001420e41981d4
Henry Januário <primeirohenry@gmail.com>
2022-07-12 18:53:50 -0300

    sprint organizando

---
+++ sprint.js
@@ -0,0 +1,19 @@
+import readlineSync from "readline-sync";
+
+let propriedades = [];
+
+while (true) {
+    
+    const input = readlineSync.question('Insira uma propriedade CSS:');
+ 
+    if (input.toUpperCase() === 'SAIR') {
+        break;
+    }
+    propriedades.push(input);
+}
+
+const organiza = propriedades.sort ((a,b)=>{
+    return a.localeCompare(b)
+})
+
+console.log(organiza);
